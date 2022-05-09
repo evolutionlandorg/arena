dc6d46c2d654ad23c86d8d12bfd208c319851def
echo <hujw77@gmail.com>
2022-05-09 12:03:07 +0800

    USDT test token

---
+++ src/USDT.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "zeppelin-solidity/access/Ownable.sol";
+import "zeppelin-solidity/token/ERC20/ERC20.sol";
+import "zeppelin-solidity/token/ERC20/extensions/ERC20Burnable.sol";
+
+contract TetherUSD is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("Tether USD", "USDT") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function decimals() public view override returns (uint8) {
+        return 6;
+    }
+}
