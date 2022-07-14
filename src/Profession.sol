ce17e676dd1f1b5a33555c50259c5bd20ace86dc
echo <hujw77@gmail.com>
2022-07-14 12:00:49 +0800

    change event

--- src/Profession.sol
+++ src/Profession.sol
@@ -8,7 +8,7 @@ import "zeppelin-solidity/token/ERC20/utils/SafeERC20.sol";
 contract Profession is DSStop {
     using SafeERC20 for IERC20;
 
-    event Buy(address indexed user, uint256 indexed amount);
+    event Buy(address indexed user, uint256 indexed amount, address token, uint fee);
 
     address public immutable USDT;
     address public immutable DEV_POOL;
@@ -24,8 +24,9 @@ contract Profession is DSStop {
     /// @param amount Amount of profession ticket to buy
     function buyProfessionTicket(uint256 amount) external stoppable {
         address user = msg.sender;
-        IERC20(USDT).safeTransferFrom(user, DEV_POOL, fee * amount);
-        emit Buy(user, amount);
+        uint totalFee = fee * amount;
+        IERC20(USDT).safeTransferFrom(user, DEV_POOL, totalFee);
+        emit Buy(user, amount, USDT, totalFee);
     }
 
     function setFee(uint256 _fee) external auth {
